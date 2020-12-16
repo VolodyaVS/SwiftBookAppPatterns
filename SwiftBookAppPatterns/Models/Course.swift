029adb1d4a1e18a916b7c3846b871d5b78571656
Volodya <volodyavolga@protonmail.ch>
2020-12-16 11:17:12 +0300

    created storyboards & model

---
+++ SwiftBookAppPatterns/Models/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  SwiftBookAppPatterns
+//
+//  Created by Vladimir Stepanchikov on 16.12.2020.
+//
+
+struct Course {
+    let name: String?
+    let imageUrl: String?
+    let numberOfLessons: Int?
+    let numberOfTests: Int?
+}
--- SwiftBookAppPatterns/ViewControllers/CourseListViewController.swift
+++ SwiftBookAppPatterns/ViewControllers/CourseListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CourseListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
