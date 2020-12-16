8fd099497a0e842c3b8f1fc5a850167ce2d8c197
Volodya <volodyavolga@protonmail.ch>
2020-12-16 09:23:38 +0300

    Initial Commit

---
+++ SwiftBookAppPatterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookAppPatterns
+//
+//  Created by Vladimir Stepanchikov on 16.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
